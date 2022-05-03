a521a2e3f8af31617af7f74da483ac3923399018
Martin Yeh <martin007004@gmail.com>
2022-05-03 14:41:10 +0800

    lab6

---
+++ lab6/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int _sum(int x, int y) {
+    return x+y;
+}
+int _sub(int x, int y) {
+    return x-y;
+}
+int _mult(int x, int y) {
+    return x*y;
+}
+int _div(int x, int y) {
+    return x/y;
+}
+int _power(int x, int y) {
+    if(y==0)return 1;
+    return _power(x,  y-1)*x;
+}
+
+int main() {
+    int x, y, choice;
+    int (*ope[4])(int, int);
+    ope[0] = _sum;
+    ope[1] = _sub;
+    ope[2] = _mult;
+    ope[3] = _div;
+    ope[4] = _power;
+    
+    printf("Enter two integer:\n");
+    scanf("%d%d", &x,&y);
+    
+    printf("Enter 0 to +, 1 to -, 2 to *, 3 to /, 4 to ^\n");
+    scanf("%d", &choice);
+    
+    printf("%d", ope[choice](x, y));
+    return 0;
+}
