b69cd09ef3dd32a33070db97fb1f523b64aa267a
avazakumo <90950178+avazakumo@users.noreply.github.com>
2022-05-31 15:00:27 +0800

    Update main.c

--- qz6/main.c
+++ qz6/main.c
@@ -1,1 +1,68 @@
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+class Clock {
+    public:
+        int hour;
+        int minute;
+        int second;
+        string timing;
+        Clock(int x, int y, int z, string s) {
+            hour = x;
+            minute = y;
+            second = z;
+            timing = s;
+        }
+        void display() {
+            cout << hour << ":" << minute << ":" << second << " " << timing << endl;
+        }
+};
+
+class StandardClock : Clock {
+    public:
+        StandardClock (int x, int y, int z, string s) : Clock(x, y, z, s) {
+            hour = x;
+            minute = y;
+            second = z;
+            timing = s;
+        }
+        void display() {
+            if (timing == "AM") {
+                cout << "上午  " << hour << ":" << minute << ":" << second << endl;
+            }else if (timing == "PM") {
+                cout << "下午  " << hour << ":" << minute << ":" << second << endl;
+            }
+        }
+            
+};
+
+class MilitaryClock : Clock {
+    public:
+        MilitaryClock (int x, int y, int z, string s) : Clock(x, y, z, s) {
+            hour = x;
+            minute = y;
+            second = z;
+            timing = s;
+        }
+        void display() {
+            if (timing == "AM") {
+                cout << hour << ":" << minute << ":" << second << endl;
+            }else if (timing == "PM") {
+                hour+=12;
+                cout << hour << ":" << minute << ":" << second << endl;
+            }
+        }
+            
+};
+
+int main() {
+    Clock cc(10, 25, 43, "AM");
+    StandardClock sc(10, 25, 43, "AM");
+    MilitaryClock mc(10, 25, 43, "PM");
+    cc.display();
+    sc.display();
+    mc.display();
+    return 0;
+}
